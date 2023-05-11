6152481cb4f2d0a72acb909d252db2eb60ed7724
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-05-11 06:15:48 +0530

    clean readme

--- add.f90
+++ add.f90
@@ -1,60 +1,22 @@
-! Project Euler #005
-! smallest +ve number divisible by all of 1-20
-recursive function gcd (m,n) result (g)
-  implicit none
-  integer, parameter :: i32 = selected_int_kind(32)
-  integer(kind=i32), intent(in) :: m,n
-  integer(kind=i32) :: g
+! Project Euler #006
+! find sum n^2 - (sum n)^2 till 100
 
-  ! Method
-  !
-  ! Euclid's algorithm
-  ! gcd(m,n) = gcd(n,m%n)
-  !
-  ! gcd(m,0) = m
-
-  if (n == 0_i32) then
-    g = m
-  else
-    g = gcd(n,mod(m,n))
-  end if
-end function gcd
-
-function lcm(m,n) result (l)
-  implicit none
-  integer, parameter :: i32 = selected_int_kind(32)
-  integer(kind=i32) gcd
-  integer(kind=i32), intent(in) :: m,n
-  integer(kind=i32) :: l
-
-  ! Method
-  !
-  ! Calculate GCD of m,n and use it to calculate LCM
-  ! LCM(m,n) = m*n/GCD(m,n)
-
-  l = m*n/gcd(m,n)
-end function lcm
-
-program ProjectEuler005
+program ProjectEuler006
   implicit none
 
   integer, parameter :: i32 = selected_int_kind(32)
-  integer(kind=i32) lcm
-  integer(kind=i32) :: n, i=20_i32
+  integer(kind=i32) :: n=100_i32
+  integer(kind=i32) :: value = 0_i32
 
   ! Method
   !
-  ! Calculate GCD of n,m and use it to calculate LCM
-  ! LCM(n,m) = n*m/GCD(n,m)
-  !
-  ! LCM(a,b,c) = LCM(LCM(a,b),c) and so on
+  ! We can use the formulas for each of them
+  ! sum n = n(n+1)/2
+  ! sum n^2 = n(n+1)(2n+1)/6
+  ! simplified
+  ! sum n^2 - (sum n)^2  = n(n+1)(3n^2 - n - 2)/12
 
-  n = 1_i32
-  do while (i > 1_i32)
-    n = lcm(n,i)
-    i = i - 1
-  end do
+  value = n*(n+1)*(3*n**2 - n - 2)/12
 
-  print *, n
-
-end program ProjectEuler005
+  print *, value
+end program ProjectEuler006
